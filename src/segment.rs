ce91a4a6addc7ea3fb5738fba6e79f7debca450e
Zack Owens <zowens2009@gmail.com>
2016-12-24 14:24:50 -0500

    Adding benchmark for message construction, bumping version

--- src/segment.rs
+++ src/segment.rs
@@ -866,4 +866,14 @@ mod tests {
             seg.append(&mut buf).unwrap();
         });
     }
+
+    #[bench]
+    fn bench_message_construct(b: &mut Bencher) {
+        b.iter(|| {
+            let mut msg_buf = MessageBuf::new();
+            msg_buf.push("719c3b4556066a1c7a06c9d55959d003d9b46273aabe2eae15ef4ba78321ae2a68b0997a4abbd035a4cdbc8b27d701089a5af63a8b81f9dc16a874d0eda0983b79c1a6f79fe3ae61612ba2558562a85595f2f3f07fab8faba1b849685b61aad6b131b7041ca79cc662b4c5aad4d1b78fb1034fafa2fe4f30207395e399c6d724");
+            msg_buf.push("2cea26f165640d448a9b89f1f871e6fca80a1255b1daea6752bf99d8c5f90e706deaecddf304b2bf5a5e72e32b29bc7c54018265d17317a670ea406fd7e6b485a19f5fb1efe686badb6599d45106b95b55695cd4e24729edb312a5dec1bc80e8d8b3ee4b69af1f3a9c801e7fb527e65f7c13c62bb37261c0");
+            msg_buf.set_offsets(1250);
+        });
+    }
 }
